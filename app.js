2deec7b7dd8f68263b2e59f8d79f8932c368b700
Paulo Ferro <paulorf87@gmail.com>
2022-12-19 10:17:02 +1100

    updates

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express"); 
 const app = express(); 
 
+
 // CUSTOM FUNCTIONS 
 const renderTest = require("./controllers/renderTest");
 
+
+// MIDDLEWARE
 app.use(express.json());
+// app.use(express.static(__dirname));
 
 
-app.get("/test", (req, res)=>{
+app.get("/", (req, res)=>{
     const response = renderTest();
+    // res.render("index.html"); 
     res.status(200).json(response);
 });
 
